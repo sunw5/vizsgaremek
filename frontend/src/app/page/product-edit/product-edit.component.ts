3532fba78511a145922c1f07253dc390448febc2
Gabor Szasz <skywallalp@gmail.com>
2022-06-10 17:40:13 +0200

    add implementation

--- frontend/src/app/page/product-edit/product-edit.component.ts
+++ frontend/src/app/page/product-edit/product-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { Observable, switchMap } from 'rxjs';
+import { Product } from 'src/app/model/product';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-product-edit',
@@ -6,10 +11,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-edit.component.scss']
 })
 export class ProductEditComponent implements OnInit {
+  product$!: Observable<Product>;
+  product: Product = new Product();
 
-  constructor() { }
+  constructor(
+    private productService: ProductService,
+    private aRoute: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.product$ = this.aRoute.params.pipe(
+      switchMap((params) => this.productService.get(params['id']))
+    );
+    this.product$.subscribe((data) => {
+      if (data) this.product = data;
+    });
+  }
+
+  onCreate(product: Product) {
+    this.productService
+      .create(product)
+      .subscribe((product) => this.router.navigate(['/', 'termekek']));
+  }
+
+  onUpdate(product: Product, form: NgForm) {
+    console.log(product);
+    this.productService
+      .update(product)
+      .subscribe((product) => this.router.navigate(['/', 'termekek']));
   }
 
 }
