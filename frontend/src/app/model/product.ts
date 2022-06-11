8fbe6ce25fb7cecd29e589dffd64812241f5397a
Gabor Szasz <skywallalp@gmail.com>
2022-06-11 11:29:50 +0200

    Edit model properties

--- frontend/src/app/model/product.ts
+++ frontend/src/app/model/product.ts
@@ -1,15 +1,12 @@
 export class Product {
   [key: string]: any;
-  _id: string = '';
-  // name: string = '';
-  // description: string = '';
-  price?: number = 0;
-  active?: boolean = true;
+  _id: string = 'new';
+  "Ár"?: number = 0;
+  "Elérhető"?: boolean = true;
   'Magyar név'?: string | null = '';
   'Latin név'?: string | null = '';
-  'Fényigény'?: 'napos' | 'árnyék' | 'félárnyék' | null = 'napos';
+  'Fényigény'?: string | null = '';
   'Tenyészidő'?: '1 nyári' | 'évelő' | null = '1 nyári';
-  // 'Talajigény'?: 'savanyú' | 'meszes' | 'normál' = 'savanyú';
   'Talajigény'?: string | null = '';
   'Habitus'?: string | null = '';
   'Teljes magasság'?: string | null = '';
