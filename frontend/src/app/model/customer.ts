edc440dfc4d38f52c851a5b6c849d2821ff77d92
Gabor Szasz <skywallalp@gmail.com>
2022-06-13 21:02:40 +0200

    Change Customer model

--- frontend/src/app/model/customer.ts
+++ frontend/src/app/model/customer.ts
@@ -1,9 +1,10 @@
 export class Customer {
   [key: string]: any;
-  id: string = '';
-  name: string = '';
-  addressBilling: string = '';
-  addressShipping: string = '';
+  _id: string = 'new';
+  firstName: string = '';
+  lastName: string = '';
+  addressBill: string = '';
+  addressShip?: string = '';
   email: string = '';
   phone: string = '';
 }
