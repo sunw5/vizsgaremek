51327394f1a30c0070cbd65a659d230894b3110a
Gabor Szasz <skywallalp@gmail.com>
2022-06-13 21:03:30 +0200

    Implenet Customer component

--- frontend/src/app/page/customer/customer.component.ts
+++ frontend/src/app/page/customer/customer.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Customer } from 'src/app/model/customer';
+import { ConfigService } from 'src/app/service/config.service';
+import { CustomerService } from 'src/app/service/customer.service';
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.scss']
+  styleUrls: ['./customer.component.scss'],
 })
 export class CustomerComponent implements OnInit {
+  columns = this.config.customerTableColumns;
 
-  constructor() { }
+  list$ = this.customerService.getAll();
 
-  ngOnInit(): void {
+  constructor(
+    private config: ConfigService,
+    private customerService: CustomerService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
+
+  onSelectOne(customer: Customer): void {
+    this.router.navigate(['/', 'vasarlo-szerkesztes', customer._id]);
   }
 
+  onDeleteOne(customer: Customer): void {
+    if (confirm('Biztosan törli ezt a vásárlót?')) {
+      this.customerService.delete(customer._id).subscribe({
+        next: () => (this.list$ = this.customerService.getAll()),
+        error: (err) => console.error(err),
+      });
+    }
+  }
 }
