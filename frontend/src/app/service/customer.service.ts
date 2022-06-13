59189d1856683c120f247f03f0eb382d96bda85d
Gabor Szasz <skywallalp@gmail.com>
2022-06-13 21:07:09 +0200

    Implement Customer service

--- frontend/src/app/service/customer.service.ts
+++ frontend/src/app/service/customer.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Customer } from '../model/customer';
+import { BaseService } from './base.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class CustomerService {
-
-  constructor() { }
+export class CustomerService extends BaseService<Customer> {
+  constructor(public override http: HttpClient) {
+    super(http);
+    this.entityName = 'customer';
+  }
 }
