b26fe797dcced48ac6910766a47203f3ac46d51b
Gabor Szasz <skywallalp@gmail.com>
2022-06-14 17:43:35 +0200

    refactor seeder to seed user properly

--- backend/src/seed/seeder.js
+++ backend/src/seed/seeder.js
@@ -2,8 +2,9 @@ const fsp = require('fs').promises;
 const { join } = require('path');
 const product = require('../models/product.model');
 const customer = require('../models/customer.model');
+const user = require('../models/user.model');
 
-const modelsToSeed = [customer];
+const modelsToSeed = [user];
 
 const seed = async () => {
   for (const model of modelsToSeed) {
@@ -16,7 +17,15 @@ const seed = async () => {
         'utf8'
       );
       const parsedData = JSON.parse(data);
-      await model.insertMany(parsedData);
+      
+      if (model.modelName.toLowerCase() === 'user') {
+        for (const userobj of parsedData) {
+          const usermodel = new model(userobj);
+          await usermodel.save();
+        }
+      } else {
+        await model.insertMany(parsedData);
+      }
       console.log(
         `Successfully seeded database with ${model.modelName.toLowerCase()}`
       );
