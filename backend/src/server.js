0a38347051a07a07b90d2a3740f749f54ec57eae
Gabor Szasz <skywallalp@gmail.com>
2022-06-14 13:25:48 +0200

    Use express.json instead of bodyParser.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const config = require('config');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -17,21 +17,22 @@ mongoose
   })
   .then((conn) => {
     console.log('Connection success!');
-    // seed(); // One time seed - uncomment to seed database     
+    // seed(); // One time seed - uncomment to seed database
   })
   .catch((err) => {
     // throw new Error(err.message);
-    console.log(err)
+    console.log(err);
   });
 
 // Cross origin resource sharing: CORS
 app.use(cors());
 app.use(express.static('public'));
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+app.use(express.json());
 
+app.use('/login', require('./controllers/login/router'));
 app.use('/product', require('./controllers/product/router'));
 app.use('/customer', require('./controllers/customer/router'));
-app.use('/login', require('./controllers/login/router'));
 
 app.use('/', (req, res) => {
   console.log(req.url);
