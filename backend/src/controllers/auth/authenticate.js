ccf7d2e177135dc298ba7f4465150fb0c5dafe90
Gabor Szasz <skywallalp@gmail.com>
2022-06-19 00:05:53 +0200

    extract token secret to variable

--- backend/src/controllers/auth/authenticate.js
+++ backend/src/controllers/auth/authenticate.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const secret = process.env.ACCESS_TOKEN_SECRET;
 
 module.exports = (req, res, next) => {
   const authHeader = req.headers.authorization;
@@ -6,8 +8,9 @@ module.exports = (req, res, next) => {
   if (authHeader) {
     // Bearer sdfdsfsdfdsfds14651651sdf65sd1f6sd1f.sdf654dsf6
     const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    jwt.verify(token, secret, (err, user) => {
       if (err) {
+        console.log('err', err);
         return res.sendStatus(401);
       }
 
