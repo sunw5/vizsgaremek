27833f3d3e950f0d54db06b447dfb85cbd4ab083
Gabor Szasz <skywallalp@gmail.com>
2022-06-22 01:21:33 +0200

    add address route

---
+++ backend/src/controllers/address/router.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+const roleGuard = require('../auth/roleGuard');
+const addressModel = require('../../models/address.model');
+const controller = require('../base/controller')(addressModel, []);
+
+
+// create
+router.post('/', roleGuard(2), (req, res, next) => {
+  return controller.create(req, res, next);
+});
+
+// read
+router.get('/', roleGuard(2), (req, res, next) => {
+  return controller.findAll(req, res, next);
+});
+
+router.get('/:id', roleGuard(2), (req, res, next) => {
+  return controller.findOne(req, res, next);
+});
+
+// update
+router.put('/:id', roleGuard(2), (req, res, next) => {
+  return controller.update(req, res, next);
+});
+
+router.patch('/:id', roleGuard(2), (req, res, next) => {
+  return controller.update(req, res, next);
+});
+
+// delete
+router.delete('/:id', roleGuard(3), (req, res, next) => {
+  return controller.delete(req, res, next);
+});
+
+module.exports = router;
+
