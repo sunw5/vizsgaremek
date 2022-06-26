675a67bc1f4a1d7281af9666e43cb2e6b902f482
Gabor Szasz <skywallalp@gmail.com>
2022-06-26 05:18:12 +0200

    add imports

--- frontend/src/app/data-table/data-table.module.ts
+++ frontend/src/app/data-table/data-table.module.ts
@@ -2,19 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxDataTableComponent } from './ngx-data-table/ngx-data-table.component';
 import { IconModule } from '../icon/icon.module';
-
-
+import { FilterPipe } from 'src/app/pipe/filter.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    NgxDataTableComponent
-  ],
-  imports: [
-    CommonModule,
-    IconModule
-  ],
-  exports: [
-    NgxDataTableComponent,
-  ],
+  declarations: [NgxDataTableComponent, FilterPipe],
+  imports: [CommonModule, IconModule, FormsModule],
+  exports: [NgxDataTableComponent],
 })
-export class DataTableModule { }
+export class DataTableModule {}
